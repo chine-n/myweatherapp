c5a8313f489b246c454f2b4a9495672c785ac017
Edu Imo <67159532+qui3tst0Rm@users.noreply.github.com>
2021-04-18 20:22:47 +0100

    Update set-version.sh

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,5 +1,5 @@
 # echo "APP_VERSION=\$(cat ~/weather-app-deployment.yaml)"
 APP_VERSION=\$(cat pom.xml | grep version | head -2 | awk -F'[<>]' '/version/ {print $3}')
-sed -i \"s/$SET_APP_VERSION/\$APP_VERSION/\" ~/weather-app-deployment.yaml
+sed -i \"s/$SET_APP_VERSION/\$APP_VERSION/\" ../weather-app-deployment.yaml
 
 
