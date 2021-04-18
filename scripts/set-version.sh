c3504d2ac5b1515807f6487f569900f8da52fc6b
Edu Imo <67159532+qui3tst0Rm@users.noreply.github.com>
2021-04-18 20:14:22 +0100

    Update set-version.sh

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,14 +1,5 @@
-echo "${mysql_address}" >> /home/ec2-user/db-address
-echo "db_address=\$(cat /home/ec2-user/db-address)" > /home/ec2-user/bootstrap.sh
-echo "sed -i \"s/localhost/\$db_address/\" /home/ec2-user/wordpress/wp-config.php" >> /home/ec2-user/bootstrap.sh
-echo "sed -i \"s/database_name_here/wpdbname/\" /home/ec2-user/wordpress/wp-config.php" >> /home/ec2-user/bootstrap.sh
-echo "sed -i \"s/username_here/wordpress/\" /home/ec2-user/wordpress/wp-config.php" >> /home/ec2-user/bootstrap.sh
-echo "sed -i \"s/password_here/Change_Me_123/\" /home/ec2-user/wordpress/wp-config.php" >> /home/ec2-user/bootstrap.sh
-chmod u+x /home/ec2-user/bootstrap.sh
-./home/ec2-user/bootstrap.sh
-sudo cp -r /home/ec2-user/wordpress/* /var/www/html/
-sudo systemctl start httpd
-sudo systemctl start node_exporter httpd
-#rm /home/ec2-user/bootstrap.sh
-#rm latest.tar.gz
-#rm -r wordpress
+# echo "APP_VERSION=\$(cat ~/weather-app-deployment.yaml)"
+APP_VERSION=\$(cat pom.xml | grep version | head -2 | awk -F'[<>]' '/version/ {print $3}')
+sed -i \"s/$SET_APP_VERSION/\$APP_VERSION/\" ~/weather-app-deployment.yaml
+
+
